0f510c4a6eb7b44a5d836f1ef3ff0d15494b4c2d
Minh Huyen Nguyen <huyennm2002@gmail.com>
2023-10-14 15:25:38 -0500

    add comments

--- p4_files/ast.hpp
+++ p4_files/ast.hpp
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <list>
 #include "tokens.hpp"
+#include "symbol_table.hpp"
 
 namespace drewno_mars {
 
@@ -14,14 +15,90 @@ class NameAnalysis;
 class SymbolTable;
 class SemSymbol;
 
-class DeclNode;
-class VarDeclNode;
-class StmtNode;
-class FormalDeclNode;
-class TypeNode;
-class ExpNode;
-class IDNode;
+/* You may find it useful to forward declare AST subclasses
+   here so that you can use a class before it's full definition
+*/
 
+//subclasses of ASTNode {{{1
+class ExpNode;
+class ProgramNode;
+class StmtNode;
+class TypeNode;
+//1}}}
+
+//subclasses of ExpNode {{{2
+class BinaryExpNode;
+class CallExpNode;
+class FalseNode;
+class IntLitNode;
+class LocNode;
+class StrLitNode;
+class TrueNode;
+class UnaryExpNode;
+//2}}}
+
+//subclasses of StmtNode {{{1
+class AssignStmtNode;
+class CallStmtNode;
+class DeclNode;
+class ExitStmtNode;
+class GiveStmtNode;
+class IfElseStmtNode;
+class IfStmtNode;
+class PostDecStmtNode;
+class PostIncStmtNode;
+class ReturnStmtNode;
+class TakeStmtNode;
+class WhileStmtNode;
+//1}}}
+
+//subclasses of TypeNode {{{1
+class BoolTypeNode;
+class ClassTypeNode;
+class IntTypeNode;
+class PerfectTypeNode;
+class VoidTypeNode;
+//1}}}
+
+//subclasses of BinaryExpNode {{{1
+class AndNode;
+class DivideNode;
+class EqualsNode;
+class GreaterEqNode;
+class GreaterNode;
+class LessEqNode;
+class LessNode;
+class MinusNode;
+class NotEqualsNode;
+class OrNode;
+class PlusNode;
+class TimesNode;
+//1}}}
+
+//subclasses of LocNode {{{1
+class IDNode;
+class MemberFieldExpNode;
+//1}}}
+
+//subclasses of UnaryExpNode {{{1
+class NegNode;
+class NotNode;
+//1}}}
+
+//subclasses of DeclNode {{{1
+class ClassDefnNode;
+class FnDeclNode;
+class VarDeclNode;
+//1}}}
+
+//subclasses of VarDeclNode {{{1
+class FormalDeclNode;
+//1}}}
+
+/** 
+* \class ASTNode
+* Base class for all other AST Node types
+**/
 class ASTNode{
 public:
 	ASTNode(const Position * pos) : myPos(pos){ }
@@ -33,6 +110,13 @@ protected:
 	const Position * myPos = nullptr;
 };
 
+/** 
+* \class ProgramNode
+* Class that contains the entire abstract syntax tree for a program.
+* Note the list of declarations encompasses all global declarations
+* which includes (obviously) all global variables and struct declarations
+* and (perhaps less obviously), all function declarations
+**/
 class ProgramNode : public ASTNode{
 public:
 	ProgramNode(std::list<DeclNode *> * globalsIn);
@@ -42,6 +126,11 @@ private:
 	std::list<DeclNode *> * myGlobals;
 };
 
+/**  \class ExpNode
+* Superclass for expression nodes (i.e. nodes that can be used as
+* part of an expression).  Nodes that are part of an expression
+* should inherit from this abstract superclass.
+**/
 class ExpNode : public ASTNode{
 protected:
 	ExpNode(const Position * p) : ASTNode(p){ }
@@ -49,12 +138,18 @@ public:
 	virtual void unparseNested(std::ostream& out);
 };
 
+/** A memory location. LocNodes subclass ExpNode
+ * because they can be used as part of an expression. 
+**/
 class LocNode : public ExpNode{
 public:
 	LocNode(const Position * p)
 	: ExpNode(p){}
 };
 
+/** An identifier. Note that IDNodes subclass
+ * LocNode because they are a type of memory location. 
+**/
 class IDNode : public LocNode{
 public:
 	IDNode(const Position * p, std::string nameIn)
@@ -69,10 +164,16 @@ private:
 	SemSymbol * mySymbol;
 };
 
+/**  \class TypeNode
+* Superclass of nodes that indicate a data type. For example, in 
+* the declaration "int a", the int part is the type node (a is an IDNode
+* and the whole thing is a DeclNode).
+**/
 class TypeNode : public ASTNode{
 public:
 	TypeNode(const Position * p) : ASTNode(p){ }
 	void unparse(std::ostream&, int) override = 0;
+	virtual std::string typeStr() = 0;
 };
 
 class StmtNode : public ASTNode{
@@ -81,6 +182,10 @@ public:
 	virtual void unparse(std::ostream& out, int indent) override = 0;
 };
 
+/** \class DeclNode
+* Superclass for declarations (i.e. nodes that can be used to 
+* declare a struct, function, variable, etc).  This base class will 
+**/
 class DeclNode : public StmtNode{
 public:
 	DeclNode(const Position * p) : StmtNode(p){ }
@@ -102,7 +207,10 @@ class VarDeclNode : public DeclNode{
 public:
 	VarDeclNode(const Position * p, IDNode * inID,
 	TypeNode * inType, ExpNode * inInit)
-	: DeclNode(p), myID(inID), myType(inType), myInit(inInit){ }
+	: DeclNode(p), myID(inID), myType(inType), myInit(inInit){
+		assert (myType != nullptr);
+		assert (myID != nullptr);
+	}
 	void unparse(std::ostream& out, int indent) override;
 	IDNode * ID(){ return myID; }
 	TypeNode * getTypeNode() const{ return myType; }
@@ -135,6 +243,7 @@ public:
 	std::list<FormalDeclNode *> * getFormals() const{
 		return myFormals;
 	}
+	TypeNode getTypeNode() { return myRetType };
 	void unparse(std::ostream& out, int indent) override;
 	virtual bool nameAnalysis(SymbolTable * symTab) override;
 private:
@@ -386,6 +495,7 @@ class VoidTypeNode : public TypeNode{
 public:
 	VoidTypeNode(const Position * p) : TypeNode(p){}
 	void unparse(std::ostream& out, int indent) override;
+	Type typeStr() override { return VOID; }
 };
 
 class ClassTypeNode : public TypeNode{
@@ -393,6 +503,7 @@ public:
 	ClassTypeNode(const Position * p, IDNode * inID)
 	: TypeNode(p), myID(inID){}
 	void unparse(std::ostream& out, int indent) override;
+	Type typeStr() override { return CHAR; }
 private:
 	IDNode * myID;
 };
@@ -411,12 +522,14 @@ public:
 	IntTypeNode(const Position * p): TypeNode(p){}
 	void unparse(std::ostream& out, int indent) override;
 	bool nameAnalysis(SymbolTable *) override;
+	Type typeStr() override { return INT; }
 };
 
 class BoolTypeNode : public TypeNode{
 public:
 	BoolTypeNode(const Position * p): TypeNode(p) { }
 	void unparse(std::ostream& out, int indent) override;
+	Type typeStr() override { return BOOL; }
 };
 
 class IntLitNode : public ExpNode{
